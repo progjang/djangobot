b094f306c59d2120085cc9c3cf4753222897883f
Prog Chang <wooritobi@gmail.com>
2017-07-27 11:21:42 +0900

    initial commit

---
+++ plusfriend/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+
+urlpatterns = [
+    url(r'^keyboard$', views.on_init),
+    url(r'^friend$', views.on_added),
+    url(r'^friend/(?P<user_key>[\w-]+)$', views.on_block),
+    url(r'^chat_room/(?P<user_key>[\w-]+)$', views.on_leave),
+    url(r'^message$', views.on_message),    
+]
